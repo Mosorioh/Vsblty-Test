b363bf803b59766930e3febecb24f62384750790
mijail devops <mijail.devops@gmail.com>
2020-05-20 19:21:55 -0700

    Refactory del codigo, pagina Engaged service

--- Run.py
+++ Run.py
@@ -215,28 +215,30 @@ print ("*******************************************************************")
 #/////////////////////////////////////////////////////////////////////////////////////////////// 
 #/////////////////////////////////////////////////////////////////////////////////////////////// 
 ################################################################################################
+
+# Funcion para Obtener la tupla de registros insertados para getFacedetection
 from getFacedetection import GetFaceDetectionService
 GetFaceDetectionService = GetFaceDetectionService (GuidTest)
 
-"""for Element in GetFaceDetectionService:
-    
-    Timeline = str(Element.get('Timeline'))
-    Frame = str(Element.get('Frame'))
-    Took = str(Element.get('Took'))
-    print (Timeline)
-    print (Frame)
-    print (Took)
-    print ("")"""
+
+################################################################################################
+#/////////////////////////////////////////////////////////////////////////////////////////////// 
+#/////////////////////////////////////////////////////////////////////////////////////////////// 
+################################################################################################
+
+# **************************************************************
 # **************************************************************
 # PDF
 # **************************************************************
+# **************************************************************
 
+# Nombre del Archivo
 FilePdfName =  GuidTest + ".pdf"
-
 doc = canvas.Canvas(FilePdfName)
+
+# Header 
 doc.setTitle("Test")
 doc.setFont("Helvetica", 10)
-
 doc.drawString(270, 785, "Test Summary")
 doc.drawString(200, 770, GuidTest)
 doc.drawString(230, 755, "FaceDetectionService Analysis took")
@@ -251,22 +253,24 @@ doc.drawString(470, 710, "Took")
 doc.line(20,700,580,700) #Creación de una linea recta
 doc.line(20,695,580,695) #Creación de una linea recta
 
-doc.setFont("Helvetica", 8)
+# //////////////////////////////////////////////////////////////////
+#  Escribir Registros en el PDF
+# //////////////////////////////////////////////////////////////////
 
 item = 1
 Salto = 40
 linea = 670
 GetFaceDetectionServiceError = []
-#
+
 for Element in GetFaceDetectionService:
 
     doc.setFont("Helvetica", 8)
     doc.setFillColorRGB(0,0,0)
-
+    # Convertimos a string los valores de cada elemento en la Tupla
     Timeline = str(Element.get('Timeline'))
     Frame = str(Element.get('Frame'))
     Took = str(Element.get('Took'))
-
+    # parametros de Busqueda y segun el valor pintar en Rojo
     OneSegundo = Took.find("00:00:01")
     TwoSegundo = Took.find("00:00:02")
     ThreeSegundo = Took.find("00:00:03")
@@ -278,42 +282,39 @@ for Element in GetFaceDetectionService:
     NineSegundo = Took.find("00:00:09") 
     TenSegundo = Took.find("00:00:10") 
 
-    
-
-    """print (OneSegundo) 
-    print (TwoSegundo) 
-    print (ThreeSegundo) 
-    print (FourSegundo) """
-
-    #
-    #doc.drawString(50, linea, str(Timeline))
+    # Escribimos en el PDf las lineas
     doc.drawString(50, linea, str(item))
     doc.drawString(100, linea, Timeline)
     
-    
+    # Si el valor de mayor a un segundo variables linea 273 se pinta en rojo
     if (OneSegundo != -1 or TwoSegundo != -1 or ThreeSegundo != -1 or FourSegundo != -1 or FiveSegundo != -1 or SixSegundo != -1 or SevenSegundo != -1 or EightSegundo != -1 or NineSegundo != -1 or TenSegundo != -1):
         doc.setFillColorRGB(92,0,0)
         doc.drawString(270, linea, Frame)
         doc.setFont("Helvetica", 10)
         doc.drawString(470, linea, Took)
+        # Elemento que entre en se guarada en la lista para pagina resumen
         GetFaceDetectionServiceError.append(Element)
     else:
         doc.setFillColorRGB(0,0,0)
         doc.setFont("Helvetica", 8)
         doc.drawString(270, linea, Frame)
         doc.drawString(470, linea, Took)
-        
-        
 
-    
+    # Definimos linea Siguiente en el PDF y Item    
     linea = linea - 15
     item += 1
+
+    # segun el Item se crea una nueva Pagina
     if (item == Salto):
 
+        # Restablesemos los valores linea y Salto
         Salto = Salto + 40
         linea = 670
+
+        # New page del PDF
         doc.showPage()
 
+        # Header Nueva Hoja del PDF
         doc.drawString(270, 785, "Test Summary")
         doc.drawString(200, 770, GuidTest)
         doc.drawString(230, 755, "FaceDetectionService Analysis took")
@@ -327,25 +328,34 @@ for Element in GetFaceDetectionService:
         doc.line(20,700,580,700) #Creación de una linea recta
         doc.line(20,695,580,695) #Creación de una linea recta
 
-# //////////////////////////////////////
+
+# ///////////////////////////////////////////
 # Pagina de Resumen
+# ///////////////////////////////////////////
+
+# New page
 doc.showPage()
 doc.setFont("Helvetica", 10)
 
+# Header
 doc.drawString(270, 785, "Test Summary")
 doc.drawString(200, 770, GuidTest)
 doc.drawString(230, 755, "FaceDetectionService Analysis took")
-doc.drawString(215, 740, "Resumen Mayor de a un Segundo ")
+doc.drawString(230, 740, "Resumen Mayor de a un Segundo ")
 
 doc.line(20,730,580,730) #Creación de una linea recta
 doc.line(20,725,580,725) #Creación de una linea recta
 doc.drawString(45, 710, "Item")
 doc.drawString(100, 710, "Time")
 doc.drawString(270, 710, "Frame")
-doc.drawString(470, 710, "Took")   
+doc.drawString(470, 710, "Took") 
+doc.line(20,700,580,700) #Creación de una linea recta
+doc.line(20,695,580,695) #Creación de una linea recta  
 
 item = 1
 linea = 670
+
+# Por cada Elemento Que demoro mas de 1 segundo se pinta resumen
 for Element in GetFaceDetectionServiceError:
 
     doc.setFont("Helvetica", 8)
@@ -364,8 +374,34 @@ for Element in GetFaceDetectionServiceError:
     doc.drawString(470, linea, Took)
     item += 1
     linea = linea - 15
-    
 
+
+################################################################################################
+#/////////////////////////////////////////////////////////////////////////////////////////////// 
+#/////////////////////////////////////////////////////////////////////////////////////////////// 
+################################################################################################   
+    
+# //////////////////////////////////////
+# Pagina Funcion Deteccion
+# //////////////////////////////////////
+
+# New Page
+doc.showPage()
+doc.setFont("Helvetica", 10)
+
+doc.drawString(270, 785, "Test Summary")
+doc.drawString(200, 770, GuidTest)
+doc.drawString(230, 755, "EngagementService Analysis took")
+#doc.drawString(230, 740, "Resumen Mayor de a un Segundo ")
+
+doc.line(20,730,580,730) #Creación de una linea recta
+doc.line(20,725,580,725) #Creación de una linea recta
+doc.drawString(45, 710, "Item")
+doc.drawString(100, 710, "Time")
+doc.drawString(270, 710, "Frame")
+doc.drawString(470, 710, "Took") 
+doc.line(20,700,580,700) #Creación de una linea recta
+doc.line(20,695,580,695) #Creación de una linea recta  
 
 
 
